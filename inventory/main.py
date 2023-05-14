6b0925da79907bd78ab0c90ce9f019fc686a8ec6
arjunsehajpal <arjunsehajpal2@gmail.com>
2023-05-14 21:12:39 +0530

    add endpoints for CRUD operations

--- inventory/main.py
+++ inventory/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
 from redis_om import HashModel
 from .utils import io_helpers
+
 import redis
 
 
 app = FastAPI()
 
-redis_conn = redis.Redis(io_helpers.get_secret("inventory/secrets.json", "redis"))
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000"], allow_methods=["*"], allow_headers=["*"])
+
+
+redis_conn_params = io_helpers.get_secret("inventory/secrets.json", "redis")
+redis_conn = redis.Redis(**redis_conn_params)
 
 
 class Product(HashModel):
@@ -18,6 +25,26 @@ class Product(HashModel):
         database = redis_conn
 
 
+def format(pk: str):
+    product = Product.get(pk)
+    return {"id": product.pk, "name": product.name, "price": product.price, "quantity": product.quantity}
+
+
 @app.get("/products")
-def all():
-    return Product.all_pks()
+async def all():
+    return [format(pk) for pk in Product.all_pks()]
+
+
+@app.get("/products/{pk}")
+def get_product(pk: str):
+    return Product.get(pk)
+
+
+@app.delete("/products/{pk}")
+def delete_product(pk: str):
+    return Product.delete(pk)
+
+
+@app.post("/products")
+def create(product: Product):
+    return product.save()
